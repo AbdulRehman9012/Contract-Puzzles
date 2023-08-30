3e0da75a296b929e7ba03c1b39fca081020fede6
AbdulRehman9012 <abdulrahmanidrees90@gmail.com>
2023-08-30 15:58:28 +0500

    Changes to be committed: 	modified:   contracts/Contract1.sol 	modified:   contracts/Contract2.sol 	modified:   contracts/Contract3.sol 	modified:   contracts/Contract4.sol 	modified:   contracts/Contract5.sol 	modified:   package-lock.json 	modified:   package.json 	modified:   test/game1Test.js 	modified:   test/game2Test.js 	modified:   test/game3Test.js 	modified:   test/game4Test.js 	modified:   test/game5Test.js

--- test/game5Test.js
+++ test/game5Test.js
@@ -1,19 +1,40 @@
 const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
 const { assert } = require('chai');
-
+const threshold = 0x00FfFFfFFFfFFFFFfFfFfffFFFfffFfFffFfFFFf;
+var address;
 describe('Game5', function () {
   async function deployContractAndSetVariables() {
     const Game = await ethers.getContractFactory('Game5');
     const game = await Game.deploy();
-
-    return { game };
+    var wallet = ethers.Wallet.createRandom(); 
+    var validAddress=false;
+    var i=0;
+      while (!validAddress) {
+      wallet = ethers.Wallet.createRandom();
+      address = await wallet.getAddress()
+      i++;
+      console.log(i);
+      console.log(await wallet.getAddress());
+      console.log(address < threshold);
+      if(address < threshold)
+       validAddress = true;
+      }
+      address = await wallet.getAddress()
+    return { game, wallet };
   }
   it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+    const { game, wallet } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
+    Wallet = wallet.connect(ethers.provider)
+    const signer = ethers.provider.getSigner(0)
 
-    await game.win();
+    // send some ether(needed for gas fees on the upcoming function call)
+    await signer.sendTransaction({
+      to: address,
+      value: ethers.utils.parseEther('200')
+    })
+    await game.connect(Wallet).win();
 
     // leave this assertion as-is
     assert(await game.isWon(), 'You did not win the game');
